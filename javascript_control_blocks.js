409fb2a204c0bf397bbd3a6312449c721c6605a0
OnesAndZer0s <37850372+OnesAndZer0s@users.noreply.github.com>
2018-11-26 15:08:18 -0600

    Update and rename control_blocks.js to javascript_control_blocks.js

---
+++ javascript_control_blocks.js
@@ -0,0 +1,17 @@
+Blockly.Blocks['event_on_event'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("on")
+        .appendField(new Blockly.FieldDropdown([["abort","abort"],["after print","afterprint"],["before print","beforeprint"],["before unload","beforeunload"],["can play","canplay"],["can play through","canplaythrough"],["change","change"],["error","error"],["fullscreen change","fullscreenchange"],["fullscreen error","fullscreenerror"],["input","input"],["invalid","invalid"],["load","load"],["loaded data","loadeddata"],["loaded metadata","loadedmetadata"],["message","message"],["offline","offline"],["online","online"],["open","open"],["pause","pause"],["play","play"],["playing","playing"],["progress","progress"],["rate change","ratechange"],["resize","resize"],["reset","reset"],["scroll","scroll"],["search","search"],["seeked","seeked"],["seeking","seeking"],["select","select"],["show","show"],["stalled","stalled"],["submit","submit"],["suspend","suspend"],["time update","timeupdate"],["toggle","toggle"],["unload","unload"],["waiting","waiting"]]), "EVENT");
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.JavaScript['event_on_event'] = function(block) {
+  var dropdown_event = block.getFieldValue('EVENT');
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'test';
+  return code;
+};
