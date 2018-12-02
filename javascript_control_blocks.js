1056380eff80cee3ac82f566e10610c3b1b5a5ab
OnesAndZer0s <37850372+OnesAndZer0s@users.noreply.github.com>
2018-12-02 17:49:02 -0600

    Update javascript_control_blocks.js

--- javascript_control_blocks.js
+++ javascript_control_blocks.js
@@ -1,21 +1,3 @@
-Blockly.Blocks['mutation_test'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("on")
-        .appendField(new Blockly.FieldDropdown([["abort","abort"],["after print","afterprint"],["before print","beforeprint"],["before unload","beforeunload"],["can play","canplay"],["can play through","canplaythrough"],["change","change"],["error","error"],["fullscreen change","fullscreenchange"],["fullscreen error","fullscreenerror"],["input","input"],["invalid","invalid"],["load","load"],["loaded data","loadeddata"],["loaded metadata","loadedmetadata"],["message","message"],["offline","offline"],["online","online"],["open","open"],["pause","pause"],["play","play"],["playing","playing"],["progress","progress"],["rate change","ratechange"],["resize","resize"],["reset","reset"],["scroll","scroll"],["search","search"],["seeked","seeked"],["seeking","seeking"],["select","select"],["show","show"],["stalled","stalled"],["submit","submit"],["suspend","suspend"],["time update","timeupdate"],["toggle","toggle"],["unload","unload"],["waiting","waiting"]]), "EVENT");
-    this.setNextStatement(true, null);
-    this.setColour(230);
-    this.setMutator("mute");
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
-Blockly.JavaScript['mutation_test'] = function(block) {
-  var dropdown_event = block.getFieldValue('EVENT');
-  // TODO: Assemble JavaScript into code variable.
-  var code = 'alert(' + dropdown_event + ');';
-  return code;
-};
 var test1 = {
   /**
    * Create XML to represent whether the 'divisorInput' should be present.
@@ -63,7 +45,25 @@ var test2 = function() {
   });
 };
 
-Blockly.Extensions.registerMutator('mute',
+Blockly.Extensions.registerMutator('test_mutator',
     test1,
     test2);
 
+Blockly.Blocks['mutation_test'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("on")
+        .appendField(new Blockly.FieldDropdown([["abort","abort"],["after print","afterprint"],["before print","beforeprint"],["before unload","beforeunload"],["can play","canplay"],["can play through","canplaythrough"],["change","change"],["error","error"],["fullscreen change","fullscreenchange"],["fullscreen error","fullscreenerror"],["input","input"],["invalid","invalid"],["load","load"],["loaded data","loadeddata"],["loaded metadata","loadedmetadata"],["message","message"],["offline","offline"],["online","online"],["open","open"],["pause","pause"],["play","play"],["playing","playing"],["progress","progress"],["rate change","ratechange"],["resize","resize"],["reset","reset"],["scroll","scroll"],["search","search"],["seeked","seeked"],["seeking","seeking"],["select","select"],["show","show"],["stalled","stalled"],["submit","submit"],["suspend","suspend"],["time update","timeupdate"],["toggle","toggle"],["unload","unload"],["waiting","waiting"]]), "EVENT");
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setMutator('test_mutator');
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.JavaScript['mutation_test'] = function(block) {
+  var dropdown_event = block.getFieldValue('EVENT');
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'alert(' + dropdown_event + ');';
+  return code;
+};
