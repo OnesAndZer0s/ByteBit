c0a9484fd33e163e12e3eb2ff10db895742bb9d9
OnesAndZer0s <37850372+OnesAndZer0s@users.noreply.github.com>
2018-12-02 13:30:09 -0600

    Update javascript_control_blocks.js

--- javascript_control_blocks.js
+++ javascript_control_blocks.js
@@ -7,7 +7,57 @@ Blockly.Blocks['mutation_test'] = {
     this.setColour(230);
  this.setTooltip("");
  this.setHelpUrl("");
-  }
+  },
+    mutationToDom: function() {
+        var a = document.createElement("mutation");
+        a.setAttribute("items", this.itemCount_);
+        return a
+    },
+    domToMutation: function(a) {
+        this.itemCount_ = parseInt(a.getAttribute("items"),
+            10);
+        this.updateShape_()
+    },
+    decompose: function(a) {
+        var b = a.newBlock("lists_create_with_container");
+        b.initSvg();
+        for (var c = b.getInput("STACK").connection, d = 0; d < this.itemCount_; d++) {
+            var e = a.newBlock("lists_create_with_item");
+            e.initSvg();
+            c.connect(e.previousConnection);
+            c = e.nextConnection
+        }
+        return b
+    },
+    compose: function(a) {
+        var b = a.getInputTargetBlock("STACK");
+        for (a = []; b;) a.push(b.valueConnection_), b = b.nextConnection && b.nextConnection.targetBlock();
+        for (b = 0; b < this.itemCount_; b++) {
+            var c = this.getInput("ADD" + b).connection.targetConnection;
+            c && -1 == a.indexOf(c) && c.disconnect()
+        }
+        this.itemCount_ = a.length;
+        this.updateShape_();
+        for (b = 0; b < this.itemCount_; b++) Blockly.Mutator.reconnect(a[b], this, "ADD" + b)
+    },
+    saveConnections: function(a) {
+        a = a.getInputTargetBlock("STACK");
+        for (var b = 0; a;) {
+            var c = this.getInput("ADD" + b);
+            a.valueConnection_ = c && c.connection.targetConnection;
+            b++;
+            a = a.nextConnection && a.nextConnection.targetBlock()
+        }
+    },
+    updateShape_: function() {
+        this.itemCount_ && this.getInput("EMPTY") ? this.removeInput("EMPTY") : this.itemCount_ || this.getInput("EMPTY") ||
+            this.appendDummyInput("EMPTY").appendField(Blockly.Msg.LISTS_CREATE_EMPTY_TITLE);
+        for (var a = 0; a < this.itemCount_; a++)
+            if (!this.getInput("ADD" + a)) {
+                var b = this.appendValueInput("ADD" + a);
+                0 == a && b.appendField(Blockly.Msg.LISTS_CREATE_WITH_INPUT_WITH)
+            } for (; this.getInput("ADD" + a);) this.removeInput("ADD" + a), a++
+    }
 };
 Blockly.JavaScript['mutation_test'] = function(block) {
   var dropdown_event = block.getFieldValue('EVENT');
