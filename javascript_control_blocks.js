7621b5dabb7640eb12f3e043a2a60c32456b97c1
OnesAndZer0s <37850372+OnesAndZer0s@users.noreply.github.com>
2018-12-02 13:52:04 -0600

    Update javascript_control_blocks.js

--- javascript_control_blocks.js
+++ javascript_control_blocks.js
@@ -5,10 +5,18 @@ Blockly.Blocks['mutation_test'] = {
         .appendField(new Blockly.FieldDropdown([["abort","abort"],["after print","afterprint"],["before print","beforeprint"],["before unload","beforeunload"],["can play","canplay"],["can play through","canplaythrough"],["change","change"],["error","error"],["fullscreen change","fullscreenchange"],["fullscreen error","fullscreenerror"],["input","input"],["invalid","invalid"],["load","load"],["loaded data","loadeddata"],["loaded metadata","loadedmetadata"],["message","message"],["offline","offline"],["online","online"],["open","open"],["pause","pause"],["play","play"],["playing","playing"],["progress","progress"],["rate change","ratechange"],["resize","resize"],["reset","reset"],["scroll","scroll"],["search","search"],["seeked","seeked"],["seeking","seeking"],["select","select"],["show","show"],["stalled","stalled"],["submit","submit"],["suspend","suspend"],["time update","timeupdate"],["toggle","toggle"],["unload","unload"],["waiting","waiting"]]), "EVENT");
     this.setNextStatement(true, null);
     this.setColour(230);
+    this.setMutator("mutation");
  this.setTooltip("");
  this.setHelpUrl("");
-  },
-    mutationToDom: function(a) {
+  }
+};
+Blockly.JavaScript['mutation_test'] = function(block) {
+  var dropdown_event = block.getFieldValue('EVENT');
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'alert(' + dropdown_event + ');';
+  return code;
+};
+var mut =  mutationToDom: function(a) {
 alert('mtd');
     },
     domToMutation: function(a) {
@@ -19,11 +27,5 @@ alert('de');
     },
     compose: function(a) {
 alert('co'); 
-    }
-};
-Blockly.JavaScript['mutation_test'] = function(block) {
-  var dropdown_event = block.getFieldValue('EVENT');
-  // TODO: Assemble JavaScript into code variable.
-  var code = 'alert(' + dropdown_event + ');';
-  return code;
-};
+    };
+Blockly.Extensions.registerMutator('mutation', mut);
