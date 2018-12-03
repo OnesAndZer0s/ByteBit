9b83e7cc9ff03953e99133eca25b264b0c86bb54
OnesAndZer0s <37850372+OnesAndZer0s@users.noreply.github.com>
2018-12-02 18:10:28 -0600

    Update javascript_control_blocks.js

--- javascript_control_blocks.js
+++ javascript_control_blocks.js
@@ -20,12 +20,70 @@ Blockly.defineBlocksWithJsonArray([
     "nextStatement": null,
     "colour": "%{BKY_LOGIC_HUE}",
     "helpUrl": "%{BKY_CONTROLS_IF_HELPURL}",
-    "mutator": "controls_if_mutator",
+    "mutator": "controls_what_mutator",
     "extensions": ["controls_if_tooltip"]
   }]);
-Blockly.JavaScript['mutation_test'] = function(block) {
-  var dropdown_event = block.getFieldValue('EVENT');
-  // TODO: Assemble JavaScript into code variable.
-  var code = 'alert(' + dropdown_event + ');';
+Blockly.JavaScript['mutation_test'] = function(block) 
+  var code = 'alert(' + 'test' + ');';
   return code;
 };
+
+Blockly.Constants.Logic.CONTROLS_TEST_MUTATOR_MIXIN = {
+  elseifCount_: 0,
+  elseCount_: 0,
+
+  /**
+   * Create XML to represent the number of else-if and else inputs.
+   * @return {Element} XML storage element.
+   * @this Blockly.Block
+   */
+  mutationToDom: function() {
+    if (!this.elseifCount_ && !this.elseCount_) {
+      return null;
+    }
+    var container = document.createElement('mutation');
+    if (this.elseifCount_) {
+      container.setAttribute('elseif', this.elseifCount_);
+    }
+    if (this.elseCount_) {
+      container.setAttribute('else', 1);
+    }
+    return container;
+  },
+  /**
+   * Parse XML to restore the else-if and else inputs.
+   * @param {!Element} xmlElement XML storage element.
+   * @this Blockly.Block
+   */
+  domToMutation: function(xmlElement) {
+    this.elseifCount_ = parseInt(xmlElement.getAttribute('elseif'), 10) || 0;
+    this.elseCount_ = parseInt(xmlElement.getAttribute('else'), 10) || 0;
+    this.updateShape_();
+  },
+  /**
+   * Populate the mutator's dialog with this block's components.
+   * @param {!Blockly.Workspace} workspace Mutator's workspace.
+   * @return {!Blockly.Block} Root block in mutator.
+   * @this Blockly.Block
+   */
+  decompose: function(workspace) {
+    var containerBlock = workspace.newBlock('controls_if_if');
+    containerBlock.initSvg();
+    var connection = containerBlock.nextConnection;
+    for (var i = 1; i <= this.elseifCount_; i++) {
+      var elseifBlock = workspace.newBlock('controls_if_elseif');
+      elseifBlock.initSvg();
+      connection.connect(elseifBlock.previousConnection);
+      connection = elseifBlock.nextConnection;
+    }
+    if (this.elseCount_) {
+      var elseBlock = workspace.newBlock('controls_if_else');
+      elseBlock.initSvg();
+      connection.connect(elseBlock.previousConnection);
+    }
+    return containerBlock;
+  };
+    
+Blockly.Extensions.registerMutator('controls_what_mutator',
+    Blockly.Constants.Logic.CONTROLS_WHAT_MUTATOR_MIXIN, null,
+    ['controls_if_elseif', 'controls_if_else']);
