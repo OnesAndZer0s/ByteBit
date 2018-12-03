dda0519b9a9f8e2818c8c1883c2431b7333c1dfd
OnesAndZer0s <37850372+OnesAndZer0s@users.noreply.github.com>
2018-12-02 18:07:15 -0600

    Update javascript_control_blocks.js

--- javascript_control_blocks.js
+++ javascript_control_blocks.js
@@ -1,15 +1,28 @@
-Blockly.Blocks['mutation_test'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("on")
-        .appendField(new Blockly.FieldDropdown([["abort","abort"],["after print","afterprint"],["before print","beforeprint"],["before unload","beforeunload"],["can play","canplay"],["can play through","canplaythrough"],["change","change"],["error","error"],["fullscreen change","fullscreenchange"],["fullscreen error","fullscreenerror"],["input","input"],["invalid","invalid"],["load","load"],["loaded data","loadeddata"],["loaded metadata","loadedmetadata"],["message","message"],["offline","offline"],["online","online"],["open","open"],["pause","pause"],["play","play"],["playing","playing"],["progress","progress"],["rate change","ratechange"],["resize","resize"],["reset","reset"],["scroll","scroll"],["search","search"],["seeked","seeked"],["seeking","seeking"],["select","select"],["show","show"],["stalled","stalled"],["submit","submit"],["suspend","suspend"],["time update","timeupdate"],["toggle","toggle"],["unload","unload"],["waiting","waiting"]]), "EVENT");
-    this.setNextStatement(true, null);
-    this.setColour(230);
-    this.setMutator('controls_if_mutator');
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+Blockly.defineBlocksWithJsonArray([
+{
+    "type": "mutation_test",
+    "message0": "%{BKY_CONTROLS_IF_MSG_IF} %1",
+    "args0": [
+      {
+        "type": "input_value",
+        "name": "IF0",
+        "check": "Boolean"
+      }
+    ],
+    "message1": "%{BKY_CONTROLS_IF_MSG_THEN} %1",
+    "args1": [
+      {
+        "type": "input_statement",
+        "name": "DO0"
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "%{BKY_LOGIC_HUE}",
+    "helpUrl": "%{BKY_CONTROLS_IF_HELPURL}",
+    "mutator": "controls_if_mutator",
+    "extensions": ["controls_if_tooltip"]
+  }]);
 Blockly.JavaScript['mutation_test'] = function(block) {
   var dropdown_event = block.getFieldValue('EVENT');
   // TODO: Assemble JavaScript into code variable.
