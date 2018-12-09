981696ac419bcd2ccdc797da6508b0339acded0d
OnesAndZer0s <37850372+OnesAndZer0s@users.noreply.github.com>
2018-12-09 14:39:14 -0600

    Update block_editor_blocks.js

--- block_editor_blocks.js
+++ block_editor_blocks.js
@@ -43,10 +43,9 @@ var mixin = {/**
    */
   compose: function(containerBlock) {
     var itemBlock = containerBlock.childBlocks;
-    console.log(containerBlock);
     // Count number of inputs.
     var connections = [];
-    while (itemBlock) {
+    while (itemBlock.length !== 0) {
       console.log(itemBlock[0])
       connections.push(itemBlock[0].type);
       itemBlock = itemBlock[0].childBlocks;
