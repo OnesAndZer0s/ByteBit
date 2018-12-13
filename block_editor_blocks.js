593c6572b76ba18571179cec92197a7760746515
OnesAndZer0s <37850372+OnesAndZer0s@users.noreply.github.com>
2018-12-13 08:01:01 -0600

    Update block_editor_blocks.js

--- block_editor_blocks.js
+++ block_editor_blocks.js
@@ -93,6 +93,7 @@ var mixin = {/**
     }
 }
 };
+editorBlockly.WorkspaceSvg.centerOnBlock('block_creator');
 Blockly.Extensions.registerMutator('creator_inputs', mixin, null, ['creator_blank_line','creator_value_line','creator_statement_line']);
 Blockly.defineBlocksWithJsonArray([
   {
