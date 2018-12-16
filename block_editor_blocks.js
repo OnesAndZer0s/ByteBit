c7c2d74b558ff29db4492302b6e47432e999857c
OnesAndZer0s <37850372+OnesAndZer0s@users.noreply.github.com>
2018-12-16 12:32:59 -0600

    Update block_editor_blocks.js

--- block_editor_blocks.js
+++ block_editor_blocks.js
@@ -88,7 +88,7 @@ var mixin = {/**
         if (this.itemCount_[i].type.split('_')[1] !== "blank") {
         var typeselect = this.workspace.newBlock('type_select');
         typeselect.setDeletable(false);
-        typeselect.setMovable(false); 
+        typeselect.setMovable(true); 
         typeselect.initSvg();
         typeselect.render();
         typeselect.outputConnection.connect(this.getInput("INFO" + i).connection);
