ea6c76db21d9e4e1e18a26c6bcf19f4773be1cef
OnesAndZer0s <37850372+OnesAndZer0s@users.noreply.github.com>
2018-12-16 12:13:11 -0600

    Update block_editor_blocks.js

--- block_editor_blocks.js
+++ block_editor_blocks.js
@@ -96,7 +96,7 @@ var mixin = {/**
     }
     // Remove deleted inputs.
      while (this.getInput('FIELD' + i)) {
-       console.log(this.getInput('INFO' + i).sourceBlock_.childBlocks_[i-4]);
+       console.log(this.getInput('INFO' + i).sourceBlock_.childBlocks_);
        if (this.getInput('INFO' + i).type == 1) {this.getInput('INFO' + i).connection}
        this.removeInput('FIELD' + i);
        i++;
